f4f5cb8483e3a47bc1eb27a77ac99975d8e3cafd
YuriSimioni <yurirs.contato@gmail.com>
2024-09-19 00:59:58 -0300

    Sistema de login funcionando

--- functions/functions.php
+++ functions/functions.php
@@ -18,16 +18,19 @@ class Usuarios {
     
     public function LoginUsuario($login, $senha) {
         global $pdo;
-        $sql = $pdo->prepare("SELECT * FROM usuario WHERE login = :l and senha = :s");
+        $sql = $pdo->prepare("SELECT * FROM usuario WHERE cpf = :c and senha = :s");
         $newPassword = hash("sha256", $senha);
-        $sql->bindValue(":l", $login);
+        
+        $sql->bindValue(":c", $login);
         $sql->bindValue(":s", $newPassword);
         $sql->execute();
         if($sql->rowCount() > 0) {
             $dadosFuncionario = $sql->fetch();
             $_SESSION['usuario'] = $dadosFuncionario;
+            $_SESSION['error'] = false;
             return true;
         } else {
+            $_SESSION['error'] = "Usuário ou senha incorretos!";
             return false;
         }
     }
